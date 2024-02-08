1bee8c293abe3998870e78217aad594307d978d9
BatBeka <bekarichman@gmail.com>
2024-02-08 15:30:25 +0400

    Add userData sub schema as 'data' in credentialChema, it has only 1 field yet and that is 'name'

--- db/connect.js
+++ db/connect.js
@@ -4,6 +4,14 @@ const mongoose = require("mongoose");
 // Connect to mongoDb with it's connection string.
 mongoose.connect(process.env.MONGO_URI);
 
+// User data schema for mongoose credential schema.
+const userData = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
 // Credential schema for mongoose.
 const credentialsSchema = new mongoose.Schema({
   // It must have email (required: true), it is string (type: String), it is unique (unique: true).
@@ -17,8 +25,13 @@ const credentialsSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
+  },
+  
+  data: {
+    type: userData,
+    required: true
   }
-})
+});
 
 // Create credential model and name it "Credentials" - (collection) based on crdential schema.
 const Credential = mongoose.model("Credentials", credentialsSchema);
